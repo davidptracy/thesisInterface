f19f731dc960dd8e9398cbce6e8571610f41925b
David Tracy <davidptracy@gmail.com>
2015-02-28 16:05:34 -0500

    modified conditional for mouse click

--- scripts/chimera.js
+++ scripts/chimera.js
@@ -12,7 +12,7 @@ function draw() {
 		var c = components[i];
 
 		if ( mouseIsPressed ){
-			if ( (mouseX > c.location.x) && ( mouseX < (640 - c.width + c.location.x) ) ){
+			if ( (mouseX > c.location.x) && ( mouseX < (c.width + c.location.x) ) ) {
 				c.setColor([255,0,0]);
 				console.log("clicking over rect!"); 
 			} else {
