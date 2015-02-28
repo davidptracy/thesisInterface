82795b2d836db5547620d9e3c584f84c72614a23
David Tracy <davidptracy@gmail.com>
2015-02-27 23:18:15 -0500

    modified chimera.js beyond default

--- scripts/chimera.js
+++ scripts/chimera.js
@@ -3,10 +3,13 @@ function setup() {
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+  // if (mouseIsPressed) {
+  //   fill(0);
+  // } else {
+  //   fill(255);
+  // }
+  // ellipse(mouseX, mouseY, 80, 80);
+
+  
+  
 }
